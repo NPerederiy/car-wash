d0bacb2c2698d39d91a7b468e9b85e3b710797dc
AlexFursenkoOd <andersen.od.ua@gmail.com>
2018-11-09 10:39:44 +0200

    - Added models - Added Service for shedule

---
+++ CarWash/Controllers/SheduleController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CarWash.Models;
+using CarWash.Services;
+
+namespace CarWash.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SheduleController : ControllerBase
+    {
+        private SheduleService _sheduleService;
+
+        public SheduleController()
+        {
+            _sheduleService = new SheduleService();
+        }
+
+        [HttpGet, Route("options")]
+        public GetWashOptionsResponse GetWashOptions()
+        {
+            return new GetWashOptionsResponse
+            {
+                WashOptions = _sheduleService.GetWashOptions()
+            };
+        }
+
+        [HttpGet, Route("awailable-days")]
+        public IEnumerable<DateTime> GetAwailableDays(GetAwailableDaysRequest request)
+        {
+             return _sheduleService.GetAwailableDays(request);
+        }
+
+        [HttpGet, Route("day-shedule")]
+        public GetSheduleForDayResponse GetSheduleForDay(GetSheduleForDayRequest request)
+        {
+            return new GetSheduleForDayResponse
+            {
+                BoxShedules = _sheduleService.GetSheduleForDay(request)
+            };
+        }
+
+
+        // GET: api/Shedule
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Shedule/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Shedule
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Shedule/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CarWash/Services/SeduleService.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarWash.Models;
+
+namespace CarWash.Services
+{
+    public class SheduleService
+    {
+        public IEnumerable<WashOption> GetWashOptions()
+        {
+            return new List<WashOption>
+            {
+                new WashOption
+                {
+                    OptionID = 1,
+                    OptionDescription = "Basic wash",
+                    Price = 5.2M,
+                    Time = 0.1
+                },
+                new WashOption
+                {
+                    OptionID = 2,
+                    OptionDescription = "Rug wash",
+                    Price = 5.2M,
+                    Time = 0.5
+                },
+                new WashOption
+                {
+                    OptionID = 3,
+                    OptionDescription = "Fully qualified washing",
+                    Price = 11.4M,
+                    Time = 1.1
+                },
+                new WashOption
+                {
+                    OptionID = 4,
+                    OptionDescription = "Windows washing",
+                    Price = 1.0M,
+                    Time = 0.1
+                },
+            };
+        }
+
+        public IEnumerable<DateTime> GetAwailableDays(GetAwailableDaysRequest request)
+        {
+            if (request.WashOptions.Count() <= 0)
+                throw new ArgumentException("At least one option should be specified");
+            var days = new List<DateTime>();
+            var now = DateTime.Now;
+            if(request.WashOptions.Count() == 1)
+            {
+                for(var i = 0; i < 31; i++)
+                {
+                    days.Add(new DateTime(now.Year, now.Month, now.Day));
+                }
+            }
+            else if(request.WashOptions.Count() < 3)
+            {
+                for (var i = 0; i < 31; i+= 2)
+                {
+                    days.Add(new DateTime(now.Year, now.Month, now.Day));
+                }
+            }
+            else 
+            {
+                for (var i = 0; i < 31; i += 5)
+                {
+                    days.Add(new DateTime(now.Year, now.Month, now.Day));
+                }
+            }
+            return days;
+        }
+
+        public IEnumerable<BoxShedule> GetSheduleForDay(GetSheduleForDayRequest request)
+        {
+            var shedules = new List<BoxShedule>();
+            var startTime = new DateTime(request.Date.Year, request.Date.Month, request.Date.Day, 9, 0, 0);
+            var endTime = new DateTime(request.Date.Year, request.Date.Month, request.Date.Day, 18, 0, 0);
+            var random = new Random();
+            for (var boxID = 0; boxID < 4; boxID++)
+            {
+                for(var time = startTime; time <= endTime; time.AddMinutes(30))
+                {
+                    var nextValue = random.Next(5000);
+                    var employeeID = random.Next(10);
+                    var boxShedule = new BoxShedule
+                    {
+                        BoxID = boxID,
+                        Day = request.Date,
+                        Time = time,
+                        OrderID = nextValue < 2400 ? nextValue : 0,
+                        EmployeeID = employeeID
+                    };
+                    shedules.Add(boxShedule);
+                }
+            }
+            return shedules;
+        }
+    }
+}
