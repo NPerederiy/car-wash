0f2b0938e4bbdaeb91dda0e6f3ae0ea3c140ec1f
Stranger-In-The-Darkness <sergeyscorpjr@gmail.com>
2018-11-22 17:08:06 +0200

    Even more functionality. Still needs some fixes

--- CarWash/wwwroot/scripts/site.js
+++ CarWash/wwwroot/scripts/site.js
@@ -1,4 +1,5 @@
-﻿const NO_API_WORK = false;
+﻿const NO_API_WORK = true;
+//const NO_API_WORK = false;
 
 const dayName = [
     "Sunday",
@@ -25,9 +26,11 @@ const monthName = [
 ];
 
 const colors = {
-    daySelected: "cccccc",
+    daySelected: "rgb(0,206,209)",
+    daySelectedText: "#ffffff",
+    dayDeselectedText: "rgb(30, 0, 230)",
     dayDeselected: "#ffffff",
-    dayHoverIn: "darkturquoise",
+    dayHoverIn: "rgba(0,206,209,0.35)",
     dayHoverOut: "#ffffff",
     today: "#4b0082",
     availableDay: "green"
@@ -39,15 +42,32 @@ const urls = {
     dayScheduleUrl: "api/schedule/day-shedule"          //Передаём список услуг и день (та же петрушка с обёектом, и объект Date, по-идее)
 };
 
+var startTime = new Date();
+startTime.setHours(9);
+startTime.setMinutes(0);
+startTime.setSeconds(0);
+
+var endTime = new Date();
+endTime.setHours(22);
+endTime.setMinutes(0);
+endTime.setSeconds(0);
+
+var timeStepMinutes = 10;
+
+
 var options = {};
 
 var hoveredOption;
 
 var selectedDay;
+
 var selectedDayEl;
+
 var selectedOptions = [];
 
+
 var totalPrice = 0;
+
 var totalTime = 0;
 
 $(document).ready(function () {
@@ -56,14 +76,15 @@ $(document).ready(function () {
 });
 
 $(".schedule").click(function (event) {
-    $(".day").css({ backgroundColor: "#ffffff" });
+    $(".day").css({ backgroundColor: colors.dayDeselected, color: colors.dayDeselectedText });
+    $(".today").css({ color: colors.today });
     selectedDay = undefined;
     selectedDayEl = undefined;
 });
 
 function changeServices() {
     if (NO_API_WORK) {
-        $.getJSON("src\services.json", function (data) {
+        $.getJSON("src/services.json", function (data) {
             const div = $("#services");
 
             $(div).empty();
@@ -74,7 +95,7 @@ function changeServices() {
             $.each(data, function (key, item) {
                 $.each(item, function (itemKey, value) {
                     options[value.optionID] = value;
-                    inner += `<li><div><div id = "service_${value.optionID}" class = "checkbox" onclick="optionToggle(this);">+</div> <div id = "descr_${value.optionID}" class = "descr" onmouseover="serviceMouseOver(this, event);" onmouseout="serviceMouseOut(this, event)">${value.optionDescription}</div></div><div id = "data_${value.optionID}" class = "data"></div></li>`;
+                    inner += `<li><div><div id = "service_${value.optionID}" class = "checkbox" onclick="optionToggle(this);">+</div> <div id = "descr_${value.optionID}" class = "descr">${value.optionDescription}</div></div><div style="margin-left: 30px;">${value.price}$ ${value.time}m</div></li>`;
                 });
                 $('.checkbox, .descr').on('click', function () {
                     getDaySchedule();
@@ -99,7 +120,7 @@ function changeServices() {
             $.each(data, function (key, item) {
                 $.each(item, function (itemKey, value) {
                     options[value.optionID] = value;
-                    inner += `<li><div><div id = "service_${value.optionID}" class = "checkbox" onclick="optionToggle(this);">+</div> <div id = "descr_${value.optionID}" class = "descr">${value.optionDescription}</div></div><div id = "data_${value.optionID}" class = "data"></div></li>`;
+                    inner += `<li><div><div id = "service_${value.optionID}" class = "checkbox" onclick="optionToggle(this);">+</div> <div id = "descr_${value.optionID}" class = "descr">${value.optionDescription}</div></div><div style="margin-left: 30px;">${options[id].price}$ ${options[id].time}m</div></li>`;
                 });
             });
 
@@ -107,24 +128,24 @@ function changeServices() {
 
             div.html(inner);
 
-            $(".descr").hover(
-                function (event) {
-                    event.stopPropagation();
-                    if (hoveredOption !== $(this).attr("id")) {
-                        hoveredOption = $(this).attr("id");
+            //$(".descr").hover(
+            //    function (event) {
+            //        event.stopPropagation();
+            //        if (hoveredOption !== $(this).attr("id")) {
+            //            hoveredOption = $(this).attr("id");
 
-                        var id = $(this).attr("id").substring(6);
+            //            var id = $(this).attr("id").substring(6);
 
-                        $(`#data_${id}`).html(`<div style="margin-left: 30px;">Price: ${options[id].price}<br>Time: ${options[id].time}</div>`);
-                    }
-                },
-                function (event) {
-                    event.stopPropagation();
-                    hoveredOption = "";
-                    var id = $(this).attr("id").substring(6);
-                    $(`#data_${id}`).html("");
-                }
-            );
+            //            $(`#data_${id}`).html(`<div style="margin-left: 30px;">Price: ${options[id].price}<br>Time: ${options[id].time}</div>`);
+            //        }
+            //    },
+            //    function (event) {
+            //        event.stopPropagation();
+            //        hoveredOption = "";
+            //        var id = $(this).attr("id").substring(6);
+            //        $(`#data_${id}`).html("");
+            //    }
+            //);
         }
     });
 }
@@ -186,33 +207,31 @@ function changeCalendar() {
      * 13*6 ячеек */
 
     html = "";
-    time = new Date();
-    time.setHours(9);
-    time.setMinutes(0);
-    for (i = 0; i < 13 * 6; i++) {
+    time = startTime;
+    while (time.getHours() < endTime.getHours()) {
         html += `\n<tr><td>
             ${time.getHours() < 10 ? "0" + time.getHours() : time.getHours()}:
             ${time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes()}
             </td>
-            <td id=box1${i} class="dayschedule free"></td>
-            <td id=box2${i} class="dayschedule free"></td>
-            <td id=box3${i} class="dayschedule free"></td>
-            <td id=box4${i} class="dayschedule free"></td>
+            <td id=box1${time.getHours() * 60 + time.getMinutes() - 9 * 60} class="dayschedule free"></td>
+            <td id=box2${time.getHours() * 60 + time.getMinutes() - 9 * 60} class="dayschedule free"></td>
+            <td id=box3${time.getHours() * 60 + time.getMinutes() - 9 * 60} class="dayschedule free"></td>
+            <td id=box4${time.getHours() * 60 + time.getMinutes() - 9 * 60} class="dayschedule free"></td>
             </tr>`;
-        time.setMinutes(time.getMinutes() + 10);
+        time.setMinutes(time.getMinutes() + timeStepMinutes);
     }
 
     $("#calendar").html(html);
 
     $(".day").hover(
         function (event) {
-            if ($(this).attr("id") != selectedDayEl) {
-                $(this).css({ backgroundColor: "darkturquoise" });
+            if ($(this).attr("id") !== selectedDayEl) {
+                $(this).css({ backgroundColor: colors.dayHoverIn });
             }
         },
         function (event) {
-            if ($(this).attr("id") != selectedDayEl) {
-                $(this).css({ backgroundColor: "#ffffff" });
+            if ($(this).attr("id") !== selectedDayEl) {
+                $(this).css({ backgroundColor: colors.dayHoverOut });
             }
         }
     );
@@ -220,8 +239,8 @@ function changeCalendar() {
     $(".day").click(function (event) {
         event.stopPropagation();
 
-        $(".day").css({ backgroundColor: "#ffffff" });
-        $(this).css({ backgroundColor: "#cccccc" });
+        $(".day").css({ backgroundColor: colors.dayDeselected, colors: colors.dayDeselectedText });
+        $(this).css({ backgroundColor: colors.daySelected, color: colors.daySelectedText });
 
         var date = $(this).attr("id").toString().split('.');
 
@@ -239,7 +258,7 @@ function changeCalendar() {
 
 function getAvailableDay(sender, event) {
     if (NO_API_WORK) {
-        $.getJSON("src\dates.json", function (data) {
+        $.getJSON("src/dates.json", function (data) {
             var div = $("td.day");
 
             console.log(data); //Массив дат
@@ -293,12 +312,163 @@ function getAvailableDay(sender, event) {
 
 function getDaySchedule(sender, event) {
     if (NO_API_WORK) {
-        $.getJSON("src\daySchedule.json", function (data) {
+        $.getJSON("src/daySchedule.json", function (data) {
             var div = $("td.schedule");
 
             console.log(data);
             $.each(data, function (key, item) {
                 //Тут должен быть код...
+            });
+
+            $(".free").hover(
+                function (event) {
+                    event.stopPropagation();
+
+                    var cells = 0;
+                    if (totalTime % timeStepMinutes !== 0) {
+                        cells = Math.trunc((totalTime + timeStepMinutes) / timeStepMinutes);
+                    }
+                    else {
+                        cells = Math.trunc(totalTime / timeStepMinutes);
+                    }
+
+                    var box = $(this).attr("id").substr(0, 4);
+                    var id = parseInt($(this).attr("id").substr(4));
+
+                    var enoughSpace = true;
+                    if ($(`#${box}${id + (cells - 1) * 10}`).length <= 0) {
+                        enoughSpace = false;
+                    }
+
+                    for (i = 0; i < cells; i++) {
+                        console.log($(`#${box}${id + i * 10}`));
+                        if ($(`#${box}${id + i * 10}`).hasClass("free") && enoughSpace) {
+                            try {
+                                var color = $(`#${box}${id + i * 10}`).css("background-color").split(',');
+
+                                var pref = color[0].substr(0, color[0].indexOf('('));
+
+                                color[0] = color[0].substr(color[0].indexOf('(') + 1);
+                                color[1] = parseInt(color[1]) + 50;
+                                color[color.length - 1] = color[color.length - 1].substring(0, color[color.length - 1].length - 1);
+
+                                var nColor = `${pref}(${color[0]}, ${color[1]}, ${color[2]}`;
+
+                                if (color[3] != undefined) {
+                                    nColor += `, ${color[3]})`;
+                                }
+                                else {
+                                    nColor += ")";
+                                }
+
+                                $(`#${box}${id + i * 10}`).css({ backgroundColor: nColor });
+                            }
+                            catch {
+                                console.log();
+                            }
+                        }
+                        else {
+                            enoughSpace = false;
+
+                            try {
+                                var color = $(`#${box}${id + i * 10}`).css("background-color").split(',');
+
+                                var pref = color[0].substr(0, color[0].indexOf('('));
+
+                                color[0] = color[0].substr(color[0].indexOf('(') + 1);
+                                color[0] = parseInt(color[0]) + 50;
+                                color[color.length - 1] = color[color.length - 1].substring(0, color[color.length - 1].length - 1);
+
+                                var nColor = `${pref}(${color[0]}, ${color[1]}, ${color[2]}`;
+
+                                if (color[3] != undefined) {
+                                    nColor += `, ${color[3]})`;
+                                }
+                                else {
+                                    nColor += ")";
+                                }
+
+                                $(`#${box}${id + i * 10}`).css({ backgroundColor: nColor });
+                            }
+                            catch {
+                                console.log();
+                            }
+                        }
+                    }
+                    if (enoughSpace) {
+                        //
+                    }
+                },
+                function (event) {
+                    event.stopPropagation();
+
+                    var cells = 0;
+                    if (totalTime % timeStepMinutes !== 0) {
+                        cells = Math.trunc((totalTime + timeStepMinutes) / timeStepMinutes);
+                    }
+                    else {
+                        cells = Math.trunc(totalTime / timeStepMinutes);
+                    }
+
+                    var box = $(this).attr("id").substr(0, 4);
+                    var id = parseInt($(this).attr("id").substr(4));
+
+                    var enoughSpace = true;
+                    if ($(`#${box}${id + (cells - 1) * 10}`).length <= 0) {
+                        enoughSpace = false;
+                    }
+
+                    for (i = 0; i < cells; i++) {
+                        if ($(`#${box}${id + i * 10}`).hasClass("free") && enoughSpace) {
+                            try {
+                                var color = $(`#${box}${id + i * 10}`).css("background-color").split(',');
+                                var pref = color[0].substr(0, color[0].indexOf('('));
+                                color[0] = color[0].substr(color[0].indexOf('(') + 1);
+                                color[1] = parseInt(color[1]) - 50;
+                                color[color.length - 1] = color[color.length - 1].substring(0, color[color.length - 1].length - 1);
+
+                                var nColor = `${pref}(${color[0]}, ${color[1]}, ${color[2]}`;
+                                if (color[3] != undefined) {
+                                    nColor += `, ${color[3]})`;
+                                }
+                                else {
+                                    nColor += ")";
+                                }
+                                console.log(nColor);
+                                $(`#${box}${id + i * 10}`).css({ backgroundColor: nColor });
+                            }
+                            catch {
+                                console.log();
+                            }
+                        }
+                        else {
+                            try {
+                                var color = $(`#${box}${id + i * 10}`).css("background-color").split(',');
+                                var pref = color[0].substr(0, color[0].indexOf('('));
+                                color[0] = color[0].substr(color[0].indexOf('(') + 1);
+                                color[0] = parseInt(color[0]) - 50;
+                                color[color.length - 1] = color[color.length - 1].substring(0, color[color.length - 1].length - 1);
+                                console.log(color);
+
+                                var nColor = `${pref}(${color[0]}, ${color[1]}, ${color[2]}`;
+                                if (color[3] != undefined) {
+                                    nColor += `, ${color[3]})`;
+                                }
+                                else {
+                                    nColor += ")";
+                                }
+                                console.log(nColor);
+                                $(`#${box}${id + i * 10}`).css({ backgroundColor: nColor });
+                            }
+                            catch {
+                                console.log();
+                            }
+                        }
+                    }
+                }
+            );
+            $(".free").click(function (event) {
+                event.stopPropagation();
             });
         });
         return;
@@ -339,6 +509,44 @@ function getDaySchedule(sender, event) {
         success: function (data) {
             console.log("Success!");
             console.log(data);
+
+            $(".free").hover(
+                function (event) {
+                    console.log($(this));
+                    event.stopPropagation();
+
+                    var cells = 0;
+                    if (totalTime % timeStepMinutes !== 0) {
+                        cells = Math.trunc((totalTime + timeStepMinutes) / timeStepMinutes);
+                    }
+                    else {
+                        cells = Math.trunc(totalTime / timeStepMinutes);
+                    }
+
+                    var box = $(this).attr("id").substr(0, 4);
+                    var id = parseInt($(this).attr("id").substr(4));
+                    var enoughSpace = true;
+                    for (i = 1; i <= cells; i++) {
+                        if (!$(`#${box}${id + i}`).hasClass("free")) {
+                            enoughSpace = false;
+                        }
+                        else {
+                            var color = $(`#${box}${id + i}`).css("background-color").split(",");
+                            console.log(color);
+                        }
+                    }
+                    if (enoughSpace) {
+                        //
+                    }
+                },
+                function (event) {
+                    event.stopPropagation();
+                }
+            );
+            $(".free").click(function (event)
+            {
+                event.stopPropagation();
+            });
         }
     });
 }
@@ -350,7 +558,7 @@ function optionToggle(sender) {
     var checked = $(sender).text() === "+" ? true : false;
 
     if (checked) {       
-        $(sender).text("-");
+        $(sender).html("&ndash;");
         selectedOptions.push(id);
 
         totalPrice += options[id].price;
@@ -358,7 +566,7 @@ function optionToggle(sender) {
     }
     else {
         selectedOptions.splice(selectedOptions.indexOf(id), 1);
-        $(sender).text("+");
+        $(sender).html("+");
         totalPrice -= options[id].price;
         totalTime -= options[id].time;
     }
